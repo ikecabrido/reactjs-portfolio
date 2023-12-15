69b08e417f6dadd54a9249dc16d56949f7198772
ikecabrido <russcabz28@gmail.com>
2023-12-15 09:06:45 +0800

    did some changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./Hero.css";
 
 const Hero = () => {
@@ -11,10 +12,8 @@ const Hero = () => {
               <h1>Hi! I'm Russell Ike Cabrido</h1>
             </Card.Title>
             <Card.Text>
-              <h3>
-                Currently studying about web development because I aspire to be
-                a web developer in the near future.
-              </h3>
+              Currently studying about web development because I aspire to be a
+              web developer in the near future.
             </Card.Text>
             <Button id="btn">Learn More</Button>
             <Button>Message me</Button>
